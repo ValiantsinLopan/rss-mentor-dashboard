590684c5a7693a1e9381833854aab7c3614b121a
Valentin Lopan <valiantsin_lopan@live.com>
2019-02-24 21:01:27 +0300

    Updates

---
+++ precache-manifest.cb8576406391644ec0a1567c4d301344.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5280a2c0b2d5e5880bdc",
+    "url": "/rss-mentor-dashboard/static/js/runtime~main.5280a2c0.js"
+  },
+  {
+    "revision": "260350cddcd7a1dab71b",
+    "url": "/rss-mentor-dashboard/static/js/main.260350cd.chunk.js"
+  },
+  {
+    "revision": "721b7117b81964c42487",
+    "url": "/rss-mentor-dashboard/static/js/2.721b7117.chunk.js"
+  },
+  {
+    "revision": "260350cddcd7a1dab71b",
+    "url": "/rss-mentor-dashboard/static/css/main.1830f7f8.chunk.css"
+  },
+  {
+    "revision": "6b013c5dde18d56a3cf842e03b1715ce",
+    "url": "/rss-mentor-dashboard/index.html"
+  }
+];
